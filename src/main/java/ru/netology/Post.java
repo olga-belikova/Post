d46335745e145977623f61bc112ccfa2f39cb19f
Olga Belikova <belko.belikova@gmail.com>
2023-01-09 21:29:12 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public Boolean subscription;
+    public FormDate birthday;
+}
